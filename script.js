2620f7184e842c2dcf8c37b56d0b8b3e9dd4453f
shubhamtiwari08 <st1173076@gmail.com>
2022-12-16 04:25:49 +0530

    updated the logic

--- script.js
+++ script.js
@@ -2,21 +2,23 @@ let translate = document.getElementById("translate-btn")
 let userInput = document.getElementById("user-ipt")
 let output = document.getElementById("output-area")
 
-translate.addEventListener("click", translation)
-
 var serverUrl = "https://api.funtranslations.com/translate/ferb-latin.json"
 
+function errorHandler(error) { console.log(`error ocurred ${error}`) }
 
-function getUrl(text) {
-    serverUrl + "?" + "text=" + text
+function getTranslationUrl(text) {
+    return serverUrl + "?" + "text=" + text
 }
 
+function clickhandler() {
+    var inputText = userInput.value
+    fetch(getTranslationUrl(inputText))
+        .then(response => response.json())
+        .then(json =>
+            output.textContent = json.contents.translated)
 
-function translation() {
-
-    let userInputValue = userInput.value
-
-    fetch(getUrl(userInputValue))
-        .then(fetch)
+        .catch(errorHandler)
 }
+
+translate.addEventListener("click", clickhandler)
 
